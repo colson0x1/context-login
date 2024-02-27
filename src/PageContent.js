bc1a7922c447fa217136c560f0cf8d0dd8c5bade
Colson <colsoncalifornication@gmail.com>
2024-02-27 15:58:52 +0545

    consume a context with static contextType

--- src/PageContent.js
+++ src/PageContent.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { ThemeContext } from './context/ThemeContext';
 
 export default class PageContent extends Component {
+  static contextType = ThemeContext;
+
   render() {
+    const { isDarkMode } = this.context;
     const styles = {
-      backgroundColor: 'white',
+      backgroundColor: isDarkMode ? 'black' : 'white',
       height: '100vh',
       width: '100vw',
     };
