94a001b1691636d1c005ebc1a3e7fc2891408723
Colson <colsoncalifornication@gmail.com>
2024-02-27 16:16:59 +0545

    create language context

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Navbar from './Navbar';
 import Form from './Form';
 import PageContent from './PageContent';
 import { ThemeContext } from './context/ThemeContext';
+import { LanguageProvider } from './context/LanguageContext';
 
 class App extends Component {
   render() {
     return (
       <ThemeContext>
-        <PageContent>
-          <Navbar />
-          <Form />
-        </PageContent>
+        <LanguageProvider>
+          <PageContent>
+            <Navbar />
+            <Form />
+          </PageContent>
+        </LanguageProvider>
       </ThemeContext>
     );
   }
